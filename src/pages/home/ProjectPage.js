b6b5858a8dc37e0282011db4c6f39d5e226ba86a
Karim-Nabarawi <K.nabarawi@gmail.com>
2022-02-15 23:06:29 +0200

    - Removed tabs - Added Slider free mode - Updated Swiper

--- src/pages/home/ProjectPage.js
+++ src/pages/home/ProjectPage.js
@@ -9,10 +9,11 @@ import CrownImg from "../../images/crown.png";
 //Styling and Animation
 import styled from "styled-components";
 import SliderShow from "../../components/utilities/SliderShow";
+import SliderGrid from "../../components/utilities/SliderGrid";
 import Button3D from "../../components/utilities/Button3D";
 import { ReactComponent as Github } from "../../images/github.svg";
 import { ReactComponent as Website } from "../../images/website.svg";
-import Tabs from "../../components/utilities/Tabs";
+import { motion } from "framer-motion";
 
 const ProjectPage = () => {
   return (
@@ -20,30 +21,26 @@ const ProjectPage = () => {
       <SliderShow images={[CaptureImg, IgniteImg, CrownImg]} />
       <DetailsContainer>
         <h1>Crown</h1>
-        <Tabs
-          data={
-            <ToolsData>
-              <ToolDetail>{getToolsData("reactJs").src}</ToolDetail>
-              <ToolDetail>{getToolsData("firebase").src}</ToolDetail>
-              <ToolDetail>{getToolsData("styledComponents").src}</ToolDetail>
-              <ToolDetail>{getToolsData("nodeJs").src}</ToolDetail>
-              <ToolDetail>{getToolsData("stripe").src}</ToolDetail>
-            </ToolsData>
-          }
-        />
         <p>E-commercal website with payment using stripe</p>
         <ToolsContainer>
-          <ToolsText>
-            <h2>Tools & packages</h2>
-          </ToolsText>
-          <ToolsData>
-            <ToolDetail>{getToolsData("reactJs").src}</ToolDetail>
-            <ToolDetail>{getToolsData("firebase").src}</ToolDetail>
-            <ToolDetail>{getToolsData("styledComponents").src}</ToolDetail>
-            <ToolDetail>{getToolsData("nodeJs").src}</ToolDetail>
-            <ToolDetail>{getToolsData("stripe").src}</ToolDetail>
+          <h2>Tools & packages</h2>
+          <ToolsData
+            whileHover={{
+              scale: 1.1,
+            }}
+          >
+            <div className="center">
+              <SliderGrid>
+                <ToolDetail>{getToolsData("reactJs").src}</ToolDetail>
+                <ToolDetail>{getToolsData("firebase").src}</ToolDetail>
+                <ToolDetail>{getToolsData("styledComponents").src}</ToolDetail>
+                <ToolDetail>{getToolsData("nodeJs").src}</ToolDetail>
+                <ToolDetail>{getToolsData("stripe").src}</ToolDetail>
+              </SliderGrid>
+            </div>
           </ToolsData>
         </ToolsContainer>
+
         <h2>Links</h2>
 
         <LinksContainer>
@@ -66,17 +63,27 @@ const DetailsContainer = styled.div`
 `;
 
 const ToolsContainer = styled.div`
-  margin: 5%;
-`;
-const ToolsText = styled.div`
-  flex: 1;
+  margin: 5% auto;
+  max-width: 800px;
 `;
 
-const ToolsData = styled.div`
+const ToolsData = styled(motion.div)`
   flex: 1;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  min-height: 200px;
+  border-radius: 10px;
+  padding: 20px;
+  margin-top: 20px;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+  transition: all 0.3s ease-in-out;
+  position: relative;
+  .center {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 100%;
+    -ms-transform: translate(-50%, -50%);
+    transform: translate(-50%, -50%);
+  }
   svg {
     width: 100%;
     min-width: 50px;
@@ -88,6 +95,22 @@ const ToolsData = styled.div`
     width: 100%;
     min-width: 50px;
     max-width: 100px;
+  }
+  &:after {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: -1;
+    width: 100%;
+    height: 100%;
+    opacity: 0;
+    border-radius: inherit;
+    box-shadow: 0px 19px 38px rgba(0, 0, 0, 0.3), 0px 15px 12px rgba(0, 0, 0, 0.22);
+    transition: 0.3s ease-in-out;
+  }
+  &:hover::after {
+    opacity: 1;
   }
 `;
 
